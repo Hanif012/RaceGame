5df50dbba54264abc6e650398f4d3989ff25c360
Hanif <andque12345012@gmail.com>
2025-03-03 23:52:49 +0700

    Fix Menu Carousel

--- Assets/Scripts/Interface/CarouselItem.cs
+++ Assets/Scripts/Interface/CarouselItem.cs
@@ -2,5 +2,9 @@ using UnityEngine;
 
 public class CarouselItem : MonoBehaviour
 {
-    
+    [Header("Item Properties")]
+    public string itemName;
+    public string itemDescription;
+    public Sprite itemIcon;
+    public enum ItemType {Locked, Unlocked, NULL};
 }
